e6fcc9424036d313c266bb7c228489a0f580d671
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-02-21 19:21:07 +0000

    hurr

---
+++ contracts/SourceIndex/deployments/2000/config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+declare \
+  -A \
+  solc_version \
+  evm_version \
+  contract_address
+
+solc_version=(
+  ["1.0"]="0.8.24"
+)
+evm_version=(
+  ["1.0"]="paris"
+)
+contract_address=(
+  ["1.0"]="0x38121895bDa4bae84bb9B0cf3843FaAf2d28222C"
+)
